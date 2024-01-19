cfe81b10cb33f6fb8a1a8466bb42416a709a78e1
Ashish Gokhale <ashishgokhalem314@gmail.com>
2024-01-20 00:12:32 +0530

    all responsive 110%

--- src/components/sidear/Sidebar.jsx
+++ src/components/sidear/Sidebar.jsx
@@ -6,80 +6,96 @@ import { IoMdMenu } from "react-icons/io";
 import { useState } from 'react';
 import { GiCrossMark } from "react-icons/gi";
 import { useEffect } from 'react';
+import { GiBattleGear } from "react-icons/gi";
+import { GiRunningNinja } from "react-icons/gi";
+import { GiLaurelsTrophy } from "react-icons/gi";
+import { GiNinjaHeroicStance } from "react-icons/gi";
 
 
 function Sidebar() {
 
-    const [menuToggle,setMenuToggle]=useState(true);
+  const [menuToggle, setMenuToggle] = useState(true);
 
 
-    useEffect(() => {
-        const handleResize = () => {
-          
-           if(window.innerWidth>1000){
 
-            setMenuToggle(true);
-          }
-          else{
-            setMenuToggle(false);
-          };
-        };
-        
-        window.addEventListener("load", handleResize);
+  useEffect((e) => {
 
-        window.addEventListener('resize', handleResize);
-    
-        // Clean up the event listener when the component unmounts
-        return () => {
-          window.removeEventListener('resize', handleResize);
-          window.removeEventListener('load', handleResize);
-        };
-      }, []);
+    menuToggle ? '' : setTimeout(() => {
+      window.document.getElementById("toggleMenus").classList.add('hideme');
+    }, 500);
+  }, [menuToggle]);
 
-      useEffect((e) => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar-title'>
+        Ashish Gokhale
+      </div>
 
-        menuToggle?'':setTimeout(()=>{
-            window.document.getElementById("toggleMenus").classList.add('hideme');
-        },500);
-      },[menuToggle]);
-
-    return (
-        <div className='sidebar'>
-              <div className='sidebar-title'>
-                Ashish Gokhale  
-            </div>
-            
-            <div className='menu-container'>
-            <div className='menu-toggle' onClick={(e)=>(setMenuToggle(menuToggle?false:true))}>
-                {menuToggle?<GiCrossMark />:<IoMdMenu />}
-            </div>
-               {
-               
-                    <div  id="toggleMenus"  className={menuToggle?'menus  slide-right':'menus slideleft'}>
-                   
-                    <div className='menu-item'>
-                        <h3><Link className='link' to="/about"><FaUserNinja/>About</Link></h3>
-                    </div>
-                    <div className='menu-item'>
-                        <h3><Link className='link' to="/experience">Work Experience</Link></h3>
-                    </div>
-                    <div className='menu-item'>
-                        <h3><Link className='link' to="/skills">Skills</Link></h3>
-                    </div>
-                    <div className='menu-item'>
-                        <h3><Link className='link' to='/certificates'>Certificates</Link></h3>
-                    </div>
-                    <div className='menu-item'>
-                        <h3><Link className='link' to='/projects'>Projects</Link></h3>
-                    </div>
-                </div>
-                
-               }
-                
-
-            </div>
+      <div className='menu-container'>
+        <div className='menu-toggle' >
         </div>
-    )
+        {
+
+          <div id="toggleMenus" className={menuToggle ? 'menus  slide-right' : 'menus slideleft'}>
+
+            <div className='menu-item'>
+            <Link className='link' to="/about">
+               <div className='dv'>
+                <h3><FaUserNinja />About</h3>
+              </div>
+              <div className='mv'>
+              <FaUserNinja />
+              </div>
+              </Link>
+            </div>
+            <div className='menu-item'>
+            <Link className='link' to="/experience">
+               <div className='dv'>
+                <h3><GiBattleGear />Experience</h3>
+              </div>
+              <div className='mv'>
+              <GiBattleGear />
+              </div>
+              </Link>
+            </div>
+            <div className='menu-item'>
+            <Link className='link' to="/skills">
+               <div className='dv'>
+                <h3><GiRunningNinja /> Skills</h3>
+              </div>
+              <div className='mv'>
+              <GiRunningNinja />
+              </div>
+              </Link>
+            </div>
+            <div className='menu-item'>
+            <Link className='link' to='/certificates'>
+               <div className='dv'>
+                <h3> <GiLaurelsTrophy /> Certificates</h3>
+              </div>
+              <div className='mv'>
+              <GiLaurelsTrophy />
+              </div>
+              </Link>
+            </div>
+            <div className='menu-item'>
+            <Link className='link' to='/projects'>
+               <div className='dv'>
+                <h3> <GiNinjaHeroicStance /> Projects</h3>
+              </div>
+              <div className='mv'>
+              <GiNinjaHeroicStance />
+              </div>
+              </Link>
+            </div>
+          </div>
+
+        }
+
+
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
